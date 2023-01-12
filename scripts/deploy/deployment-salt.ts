86c9e3dbb31a756b3b8d864a2bdfc5338e11c740
Ankur Dubey <ankurdubey521@protonmail.com>
2023-01-12 17:48:31 +0530

    Update contract addresses

--- scripts/deploy/deployment-salt.ts
+++ scripts/deploy/deployment-salt.ts
@@ -2,7 +2,7 @@ export enum DEPLOYMENT_SALTS {
   DiamondInit = 'DIAMOND_INIT_V1',
   CCMPConfigurationFacet = 'CCMP_CONFIGURATION_FACET_V2',
   CCMPReceiverMessageFacet = 'CCMP_RECEIVER_MESSAGE_FACET_V1',
-  CCMPSendMessageFacet = 'CCMP_SEND_MESSAGE_FACET_V1',
+  CCMPSendMessageFacet = 'CCMP_SEND_MESSAGE_FACET_V2',
   DiamondCutFacet = 'DIAMOND_CUT_FACET_V1',
   DiamondLoupeFacet = 'DIAMOND_LOUPE_FACET_V1',
   Diamond = 'DIAMOND_V1',
